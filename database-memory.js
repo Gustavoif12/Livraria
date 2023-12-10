412b7be2853ac79c305c404a32feecb6d1f8b36d
Gustavo Silva <118355959+Gustavoif12@users.noreply.github.com>
2023-12-09 21:00:37 -0400

    primeiro commit

---
+++ database-memory.js
@@ -0,0 +1,38 @@
+import { randomUUID } from "crypto"
+
+export class DatabaseMemory {
+  #mangas = new Map()
+
+  list(search) {
+    return Array.from(this.#mangas.entries()).map((mangaArray) => {
+      const id = mangaArray[0]
+
+      const data = mangaArray[1]
+
+      return {
+        id,
+        ...data,
+      }
+
+    })
+      .filter(manga => {
+        if (search) {
+          return manga.titulo.includes(search)
+        }
+        return true
+      })
+  }
+
+  create(manga) {
+    const mangaId = randomUUID()
+    this.#mangas.set(mangaId, manga)
+  }
+
+  update(id, manga) {
+    this.#mangas.set(id, manga)
+  }
+
+  delete(id, manga) {
+    this.#mangas.delete(id, manga)
+  }
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+import { fastify } from 'fastify'
+import { DatabaseMemory } from "./database-memory.js"
+
+const server = fastify()
+const database = new DatabaseMemory()
+
+server.get('/', () => {
+  return 'Olá Mundo!'
+})
+
+server.post('/manga', (request, reply) => {
+  const { titulo, mangaka, quantidade_volume } = request.body
+  database.create({
+    titulo: titulo,
+    mangaka: mangaka,
+    quantidade_volume: quantidade_volume
+  })
+  console.log(database.list())
+  return reply.status(201).send()
+})
+
+server.get('/manga', (request) => {
+  const search = request.query.search
+
+  console.log(search)
+
+  const mangas = database.list(search)
+
+  return mangas
+})
+
+server.put('/manga/:id', (request, reply) => {
+  const mangaId = request.params.id
+  const { titulo, mangaka, quantidade_volume } = request.body
+  const manga = database.update(mangaId, {
+    titulo,
+    mangaka,
+    quantidade_volume,
+  })
+  return reply.status(204).send()
+})
+
+server.delete('/manga/:id', (request, reply) => {
+  const mangaId = request.params.id
+
+  database.delete(mangaId)
+
+  return reply.status(204).send()
+})
+
+server.listen({
+  port: 3333,
+})
